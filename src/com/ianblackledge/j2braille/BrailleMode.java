b28724d392983b6dce9896d5a06a472c87584118
ianblackledge <ianblackledge@users.noreply.github.com>
2018-12-19 08:52:06 -0700

    Added copyright notices

--- src/com/ianblackledge/j2braille/BrailleMode.java
+++ src/com/ianblackledge/j2braille/BrailleMode.java
@@ -1,3 +1,10 @@
+/***********************************************************
+ * J2Braille - Copyright (c) 2018 Ian Blackledge
+ *
+ * Licenced under the GNU General Public License v3.0
+ * (Please see the license document for details.)
+ **********************************************************/
+
 package com.ianblackledge.j2braille;
 
 enum BrailleMode {
